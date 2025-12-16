92bd02312471872cceb5ca2ed4011c28ca563a01
almeidadylan <almeidadylan793@gmail.com>
2025-12-15 21:17:31 -0400

    feat: adicionando test da versão do postgres chumbado

--- pages/api/v1/postgres/index.js
+++
@@ -1,8 +0,0 @@
-async function postgresVersion(req, res) {
-  const psqlVersion =
-    (await process.env.PG_VERSION) || "16.11 - resposta padrão";
-
-  res.status(200).json({ postgres_version: psqlVersion });
-}
-
-export default postgresVersion;
