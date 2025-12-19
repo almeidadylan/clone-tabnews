8dd74b90287d61b21f3940f107b16835aac281b1
almeidadylan <almeidadylan793@gmail.com>
2025-12-18 20:27:37 -0400

    feat: adicionando teste da resposta da função getPostgresVersion

--- tests/integration/api/v1/status/postgres/get.test.js
+++ tests/integration/api/v1/status/postgres/get.test.js
@@ -6,6 +6,20 @@ test("GET to api/v1/status/postgres should return 200", async () => {
   expect(responseBody.postgres_version).toBeDefined();
   console.log(responseBody);
 
-  const expectedVersion = "16.0";
-  expect(responseBody.postgres_version).toEqual(expectedVersion);
+  const expectedVersionSimple = "16.0";
+  expect(responseBody.postgres_version.simple_version).toEqual(
+    expectedVersionSimple
+  );
+
+  const expectedVersionFull =
+    "PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit";
+  expect(responseBody.postgres_version.full_version).toEqual(
+    expectedVersionFull
+  );
+
+  const espectedFullResponse = {
+    simple_version: expectedVersionSimple,
+    full_version: expectedVersionFull,
+  };
+  expect(responseBody.postgres_version).toEqual(espectedFullResponse);
 });
