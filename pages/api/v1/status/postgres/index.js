e4a6643aeeabeeace8ef7cd67f42b35750c2d50b
almeidadylan <almeidadylan793@gmail.com>
2025-12-18 20:26:49 -0400

    refactor: refatorando resposta da função getPostgresVersion

--- pages/api/v1/status/postgres/index.js
+++ pages/api/v1/status/postgres/index.js
@@ -8,33 +8,35 @@ const pool = new Pool({
   port: process.env.POSTGRES_PORT,
 });
 
-async function postgresVersion(req, res) {
-  const psqlVersion =
-    (await getPostgresVersion()) || "erro na resposta ";
+async function postgresVersion(req, res, simple) {
+  const psqlVersion = (await getPostgresVersion(simple)) || "erro na resposta ";
 
   res.status(200).json({ postgres_version: psqlVersion });
 }
 
 export default postgresVersion;
 
-
-
-
 async function getPostgresVersion() {
   let client;
   try {
     client = await pool.connect();
 
-    const result = await client.query('SELECT version();');
+    const result = await client.query("SELECT version();");
 
     const versionString = result.rows[0].version;
-    console.log('Versao do postgres:', versionString);
-    return format(versionString);
+    console.log("Versao do postgres:", versionString);
+    
+    return {
+      simple_version: format(versionString),
+      full_version: versionString
+    }
+    
 
+    return versionString;
   } catch (err) {
-    console.erro('Erro ao buscart a versão do PostgresSQL:', err);
+    console.erro("Erro ao buscart a versão do PostgresSQL:", err);
 
-    throw new Error('Falha na comunicação com o banco de dados');
+    throw new Error("Falha na comunicação com o banco de dados");
   } finally {
     if (client) {
       client.release();
@@ -42,9 +44,7 @@ async function getPostgresVersion() {
   }
 }
 
-
-
 function format(string) {
-  let result = string.split(' ');
+  let result = string.split(" ");
   return result[1];
 }
