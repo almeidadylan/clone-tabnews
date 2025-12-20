faf97cf16a40b8ecca39e553258bbf6009cc035f
almeidadylan <almeidadylan793@gmail.com>
2025-12-19 21:02:21 -0400

    feat: adicionando teste da rota `/api/v1/status/`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,16 @@
 import database from "infra/database.js";
 import getPostgresVersion from "/pages/api/v1/status/postgres/version/getPostgresVersion.js";
+import getMaxConnections from "./postgres/max_connections/maxConections";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const postgres_Version = await getPostgresVersion();
+  const max_connections = await getMaxConnections();
 
   response.status(200).json({
     updated_at: updatedAt,
     postgres_version: postgres_Version,
+    db_max_connections: max_connections,
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,8 +4,25 @@ test("Get to apt/v1/status should return 200", async () => {
 
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.postgres_version).toBeDefined();
 
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  const psqlVersionResponse = {
+    simple_version: "16.0",
+    full_version:
+      "PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit",
+  };
+  expect(responseBody.postgres_version).toBeDefined();
+  expect(responseBody.postgres_version.simple_version).toBeDefined();
+  expect(responseBody.postgres_version.simple_version).toEqual("16.0");
+  expect(responseBody.postgres_version.full_version).toBeDefined();
+  expect(responseBody.postgres_version.full_version).toEqual(
+    "PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit"
+  );
+
+  const maxConnectionsExpected = 100;
+  expect(responseBody.db_max_connections).toBeDefined();
+  expect(responseBody.db_max_connections).toBe("100"); //.toEqual("100");
+
 });
