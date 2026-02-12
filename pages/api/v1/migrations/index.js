9efbc7e9656e29916315e7931fa43d148d46b68c
almeidadylan <almeidadylan793@gmail.com>
2026-02-12 18:00:19 -0400

    corrigindo o erro do comando `test:watch` faltava `--` na flag `--runInBand`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,16 +2,6 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 async function migrations(request, response) {
-  console.log("NODE_ENV: ", process.env.NODE_ENV);
-  console.log("process.env envionment variables:", {
-    process_env: process.env.NODE_ENV,
-    postgres_host: process.env.POSTGRES_HOST,
-    postgres_db: process.env.POSTGRES_DB,
-    postgres_user: process.env.POSTGRES_USER,
-    postgres_password: process.env.POSTGRES_PASSWORD,
-    postgres_port: process.env.POSTGRES_PORT,
-    database_url: process.env.DATABASE_URL,
-  });
   if (request.method === "GET") {
     console.log("Entrou no GET");
 
